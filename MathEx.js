d29dd70d379b37f3d81eeec8dc814b401097514e
yoichi kobayashi <info@tplh.net>
2017-01-25 16:48:30 +0900

    [add] new methods clamp and mix to MathEx Class.

--- MathEx.js
+++ MathEx.js
@@ -7,6 +7,12 @@ module.exports = class MathEx {
   radians(degree) {
     return degree * Math.PI / 180;
   }
+  clamp(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+  }
+  mix(x1, x2, a) {
+    return x1 * (1 - a) + x2 * a;
+  }
   polar: function(radian1, radian2, radius) {
     return [
       Math.cos(radian1) * Math.cos(radian2) * r,
