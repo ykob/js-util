9c8ed4d4e4ecd956f2d777bcd8794d78a653c5e1
yoichi kobayashi <info@tplh.net>
2017-01-26 14:39:02 +0900

    [modify] replace y and z at the polar method.

--- MathEx.js
+++ MathEx.js
@@ -16,8 +16,8 @@ module.exports = class MathEx {
   polar: function(radian1, radian2, radius) {
     return [
       Math.cos(radian1) * Math.cos(radian2) * radius,
-      Math.cos(radian1) * Math.sin(radian2) * radius,
       Math.sin(radian1) * radius,
+      Math.cos(radian1) * Math.sin(radian2) * radius,
     ];
   }
 }
