ada0b7147ed2c4cd468951a8cf3a40d8a936cb95
yoichi kobayashi <info@tplh.net>
2017-01-27 15:52:13 +0900

    [modify] remove jQuery object from FixBody class.

--- FixBody.js
+++ FixBody.js
@@ -1,21 +1,15 @@
 module.exports = class FixBody {
   constructor() {
-    this.$window = $(window);
-    this.$body = $('body');
     this.top = 0;
   }
   set() {
-    this.top = this.$window.scrollTop()
-    this.$body.css({
-      position: 'fixed',
-      marginTop: this.top * -1
-    });
+    this.top = window.pageYOffset;
+    document.body.style.position = 'fixed';
+    document.body.style.marginTop = `${this.top * -1}px`;
   }
   cancel() {
-    this.$body.css({
-      position: 'static',
-      marginTop: 0
-    });
-    this.$window.scrollTop(this.top)
+    document.body.style.position = 'static';
+    document.body.style.marginTop = 0;
+    window.scrollTo(0, this.top);
   }
 }
