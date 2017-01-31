d60a7a8dae3ae81d3991998610f76f81c75c0ed9
yoichi kobayashi <info@tplh.net>
2017-02-01 01:01:09 +0900

    [fix] syntax error.

--- MathEx.js
+++ MathEx.js
@@ -13,7 +13,7 @@ module.exports = class MathEx {
   mix(x1, x2, a) {
     return x1 * (1 - a) + x2 * a;
   }
-  polar: function(radian1, radian2, radius) {
+  polar(radian1, radian2, radius) {
     return [
       Math.cos(radian1) * Math.cos(radian2) * radius,
       Math.sin(radian1) * radius,
