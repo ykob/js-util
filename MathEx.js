72c1ea621ba2361f1bfe3a769e1318d739e4f3f5
yoichi kobayashi <info@tplh.net>
2017-02-01 01:05:53 +0900

    [fix] syntax error.

--- MathEx.js
+++ MathEx.js
@@ -1,17 +1,17 @@
 module.exports = {
-  degrees(radian) {
+  degrees: function(radian) {
     return radian / Math.PI * 180;
-  }
-  radians(degree) {
+  },
+  radians: function(degree) {
     return degree * Math.PI / 180;
-  }
-  clamp(value, min, max) {
+  },
+  clamp: function(value, min, max) {
     return Math.min(Math.max(value, min), max);
-  }
-  mix(x1, x2, a) {
+  },
+  mix: function(x1, x2, a) {
     return x1 * (1 - a) + x2 * a;
-  }
-  polar(radian1, radian2, radius) {
+  },
+  polar: function(radian1, radian2, radius) {
     return [
       Math.cos(radian1) * Math.cos(radian2) * radius,
       Math.sin(radian1) * radius,
