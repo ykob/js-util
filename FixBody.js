f559a54d522e2d31343d054ab8e3158e24b49f15
yoichi kobayashi <info@tplh.net>
2017-02-16 03:32:21 +0900

    [edit] rewrite es5.

--- FixBody.js
+++ FixBody.js
@@ -1,15 +1,14 @@
-module.exports = class FixBody {
-  constructor() {
-    this.top = 0;
-  }
-  set() {
-    this.top = window.pageYOffset;
-    document.body.style.position = 'fixed';
-    document.body.style.marginTop = this.top * -1 + 'px';
-  }
-  cancel() {
-    document.body.style.position = 'static';
-    document.body.style.marginTop = 0;
-    window.scrollTo(0, this.top);
-  }
+var FixBody = function () {
+  this.top = 0;
 }
+FixBody.prototype.set = function() {
+  this.top = window.pageYOffset;
+  document.body.style.position = 'fixed';
+  document.body.style.marginTop = this.top * -1 + 'px';
+}
+FixBody.prototype.cancel = function() {
+  document.body.style.position = 'static';
+  document.body.style.marginTop = 0;
+  window.scrollTo(0, this.top);
+}
+module.exports = FixBody;
