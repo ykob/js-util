8a603609f4cddde929f7a092c63eac79cc6dcec2
yoichi kobayashi <info@tplh.net>
2017-05-12 13:16:21 +0900

    clean up.

--- FixBody.js
+++ FixBody.js
@@ -1,14 +1,14 @@
 var FixBody = function () {
   this.top = 0;
-}
-FixBody.prototype.set = function() {
-  this.top = window.pageYOffset;
-  document.body.style.position = 'fixed';
-  document.body.style.marginTop = this.top * -1 + 'px';
-}
-FixBody.prototype.cancel = function() {
-  document.body.style.position = 'static';
-  document.body.style.marginTop = 0;
-  window.scrollTo(0, this.top);
+  this.set = function() {
+    this.top = window.pageYOffset;
+    document.body.style.position = 'fixed';
+    document.body.style.marginTop = this.top * -1 + 'px';
+  }
+  this.cancel = function() {
+    document.body.style.position = 'static';
+    document.body.style.marginTop = 0;
+    window.scrollTo(0, this.top);
+  }
 }
 module.exports = FixBody;
