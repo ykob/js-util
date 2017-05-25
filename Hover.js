c67dd7ef71d31d4218edd44d3da1503e89b3aa66
yoichi kobayashi <info@tplh.net>
2017-05-25 19:27:37 +0900

    [modify] update touch events on Hover class.

--- Hover.js
+++ Hover.js
@@ -6,13 +6,13 @@ module.exports = function(elm) {
   this.elm.addEventListener('touchstart', function() {
     this.isTouched = true;
   }, false);
+  this.elm.addEventListener('touchstartend', function() {
+    this.isTouched = true;
+  }, false);
   this.elm.addEventListener('mouseover', function() {
-    if (this.isTouched) {
-      this.isTouched = false;
-    } else {
-      self.elm.classList.remove('is-leave');
-      self.elm.classList.add('is-over');
-    }
+    if (this.isTouched) return;
+    self.elm.classList.remove('is-leave');
+    self.elm.classList.add('is-over');
   }, false);
   this.elm.addEventListener('mouseleave', function() {
     if (this.isTouched) return;
