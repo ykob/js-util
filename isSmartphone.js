8a49f9d74da153219fe46d9466c0789bc2e8badd
yoichi kobayashi <info@tplh.net>
2017-06-05 11:44:16 +0900

    [modify] change strings of user agent on conditional branch "iPad" to "iPod".

--- isSmartphone.js
+++ isSmartphone.js
@@ -1,4 +1,4 @@
 module.exports = function() {
   var ua = navigator.userAgent;
-  return (ua.indexOf("iPhone") >= 0 || ua.indexOf("iPad") >= 0 || ua.indexOf("Android") >= 0);
+  return (ua.indexOf("iPhone") >= 0 || ua.indexOf("iPod") >= 0 || ua.indexOf("Android") >= 0);
 }
