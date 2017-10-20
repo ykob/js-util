49f44cd56916991f158471563d48943bb85b4b63
yoichi kobayashi <info@tplh.net>
2017-10-20 22:31:05 +0900

    [remove] removed modules that are less use frequency.

--- animate.js
+++
@@ -1,6 +0,0 @@
-module.exports = function($elm, callback) {
-  $elm.addClass('is-animate').on('transitionend', function() {
-    if (callback) callback();
-    $elm.addClass('is-animate-fix').off('transitionend');
-  });
-}
--- jquery.scrollInnerPage.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(top, duration, easing) {
-  $('html, body').animate({
-    scrollTop: top
-  }, duration, easing);
-}
