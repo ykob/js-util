c7f841fbd9d6ccccba77b23b663672a314618717
yoichi kobayashi <info@tplh.net>
2017-11-07 19:16:20 +0900

    [update] ScrollInnerPage class became to not have link element.

--- ScrollInnerPage.js
+++ ScrollInnerPage.js
@@ -6,15 +6,13 @@ var easeExponentialInOut = function(t) {
       : -0.5 * Math.pow(2.0, 10.0 - (t * 20.0)) + 1.0;
 }
 
-module.exports = function(elm, duration) {
+module.exports = function() {
   var self = this;
-  this.elm = elm;
-  this.target = document.getElementById(elm.getAttribute('href').replace('#', ''));
   this.scrollTopBefore = 0;
   this.scrollTopAfter = 0;
   this.timeClick = 0;
   this.isScrolling = false;
-  this.duration = (duration) ? duration : 1000;
+  this.duration = 0;
 
   this.render = function() {
     var time = Date.now() - self.timeClick;
@@ -25,15 +23,13 @@ module.exports = function(elm, duration) {
       self.isScrolling = false;
     }
   }
-
-  this.elm.addEventListener('click', function(event) {
-    event.preventDefault();
+  this.start = function(anchorY, duration) {
     if (self.isScrolling) return;
-    var targetRect = self.target.getBoundingClientRect();
+    this.duration = (duration) ? duration : 1000;
     self.timeClick = Date.now();
     self.isScrolling = true;
     self.scrollTopBefore = window.pageYOffset;
-    self.scrollTopAfter = self.scrollTopBefore + targetRect.top;
+    self.scrollTopAfter = anchorY;
     self.render();
-  }, false)
+  }
 }
