79796318f3c4e7dde579d8d35168df4dfa085a6b
yoichi kobayashi <info@tplh.net>
2018-03-27 23:38:26 +0900

    add easingFunc argument into ScrollInnerPage class.

--- ScrollInnerPage.js
+++ ScrollInnerPage.js
@@ -1,11 +1,3 @@
-var easeExponentialInOut = function(t) {
-  return t == 0.0 || t == 1.0
-    ? t
-    : t < 0.5
-      ? +0.5 * Math.pow(2.0, (20.0 * t) - 10.0)
-      : -0.5 * Math.pow(2.0, 10.0 - (t * 20.0)) + 1.0;
-}
-
 module.exports = function() {
   var self = this;
   this.scrollTopBefore = 0;
@@ -13,19 +5,23 @@ module.exports = function() {
   this.timeClick = 0;
   this.isScrolling = false;
   this.duration = 0;
+  this.easingFunc = null;
 
   this.render = function() {
     var time = Date.now() - self.timeClick;
-    window.scrollTo(0, self.scrollTopBefore + (self.scrollTopAfter - self.scrollTopBefore) * Math.min(easeExponentialInOut(time / self.duration, 1)));
+    window.scrollTo(0, self.scrollTopBefore + (self.scrollTopAfter - self.scrollTopBefore) * Math.min(self.easingFunc(time / self.duration, 1)));
     if (time < self.duration) {
       requestAnimationFrame(self.render);
     } else {
       self.isScrolling = false;
     }
   }
-  this.start = function(anchorY, duration) {
+  this.start = function(anchorY, duration, easingFunc) {
     if (self.isScrolling) return;
     this.duration = (duration) ? duration : 1000;
+    this.easingFunc = (easingFunc) ? easingFunc : function(t) {
+      return t;
+    };
     self.timeClick = Date.now();
     self.isScrolling = true;
     self.scrollTopBefore = window.pageYOffset;
