790e17257f348572a7fba89a6787df2e20bfedac
Yoichi Kobayashi <info@tplh.net>
2022-03-05 19:47:10 +0900

    change the way to import/export MathEx methods.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import debounce from './modules/debounce'
+import MathEx from './modules/MathEx'
 import sleep from './modules/sleep'
 import splitNum from './modules/splitNum'
 import zeroPadding from './modules/zeroPadding'
 
 export {
   debounce,
+  MathEx,
   sleep,
   splitNum,
   zeroPadding,
--- src/modules/MathEx.ts
+++ src/modules/MathEx.ts
@@ -1,37 +1,45 @@
-const MathEx = {
-  clamp: (value: number, min: number, max: number): number => {
-    return Math.min(Math.max(value, min), max)
-  },
-  degrees: (radian: number): number => {
-    return radian / Math.PI * 180
-  },
-  mix: (x1: number, x2: number, a: number): number => {
-    return x1 * (1 - a) + x2 * a
-  },
-  smoothstep: (e0: number, e1: number, x: number): number => {
-    if (e0 >= e1) return 0
-    var t = Math.min(Math.max((x - e0) / (e1 - e0), 0), 1)
-    return t * t * (3 - 2 * t)
-  },
-  spherical: (radian1: number, radian2: number, radius: number): [number, number, number] => {
-    return [
-      Math.cos(radian1) * Math.cos(radian2) * radius,
-      Math.sin(radian1) * radius,
-      Math.cos(radian1) * Math.sin(radian2) * radius,
-    ]
-  },
-  radians: (degree: number): number => {
-    return degree * Math.PI / 180
-  },
-  randomArbitrary: (min: number, max: number): number => {
-    return Math.random() * (max - min) + min
-  },
-  randomInt: (min: number, max: number): number => {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-  },
-  step: (e: number, x: number): number => {
-    return (x >= e) ? 1 : 0
-  },
+export const clamp = (value: number, min: number, max: number): number => {
+  return Math.min(Math.max(value, min), max)
+}
+export const degrees = (radian: number): number => {
+  return radian / Math.PI * 180
+}
+export const mix = (x1: number, x2: number, a: number): number => {
+  return x1 * (1 - a) + x2 * a
+}
+export const smoothstep = (e0: number, e1: number, x: number): number => {
+  if (e0 >= e1) return 0
+  var t = Math.min(Math.max((x - e0) / (e1 - e0), 0), 1)
+  return t * t * (3 - 2 * t)
+}
+export const spherical = (radian1: number, radian2: number, radius: number): [number, number, number] => {
+  return [
+    Math.cos(radian1) * Math.cos(radian2) * radius,
+    Math.sin(radian1) * radius,
+    Math.cos(radian1) * Math.sin(radian2) * radius,
+  ]
+}
+export const radians = (degree: number): number => {
+  return degree * Math.PI / 180
+}
+export const randomArbitrary = (min: number, max: number): number => {
+  return Math.random() * (max - min) + min
+}
+export const randomInt = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+export const step = (e: number, x: number): number => {
+  return (x >= e) ? 1 : 0
 }
 
-export default MathEx
+export default {
+  clamp,
+  degrees,
+  mix,
+  smoothstep,
+  spherical,
+  radians,
+  randomArbitrary,
+  randomInt,
+  step,
+}
