dbbd6c8cfd36a0674ef8b446f1b92375651e1001
Yoichi Kobayashi <info@tplh.net>
2022-03-05 22:13:16 +0900

    added the tests about degrees and radians.

--- src/test/MathEx.spec.ts
+++ src/test/MathEx.spec.ts
@@ -1,7 +1,17 @@
 import {
   clamp,
+  degrees,
+  radians,
 } from '../modules/MathEx'
 
 test('clamp', () => {
   expect(clamp(1.5, 0, 1)).toBe(1)
+})
+
+test('degrees', () => {
+  expect(degrees(Math.PI * 0.5)).toBe(90)
+})
+
+test('radians', () => {
+  expect(radians(90)).toBe(Math.PI * 0.5)
 })
