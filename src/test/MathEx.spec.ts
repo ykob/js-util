1a2b1d12e3a9f3b93d670b02542a77ca014780bb
Yoichi Kobayashi <info@tplh.net>
2022-03-06 11:32:55 +0900

    added some tests for MathEx.clamp.

--- src/test/MathEx.spec.ts
+++ src/test/MathEx.spec.ts
@@ -4,8 +4,44 @@ import {
   radians,
 } from '../modules/MathEx'
 
-test('clamp', () => {
-  expect(clamp(1.5, 0, 1)).toBe(1)
+describe('clamp', () => {
+  describe('range is from 0 to 1', () => {
+    const min = 0
+    const max = 1
+
+    test('value is less than 0.', () => {
+      expect(clamp(-1, min, max)).toBe(min)
+    })
+  
+    test('value is more than 1.', () => {
+      expect(clamp(2, min, max)).toBe(max)
+    })
+
+    test('value is within the range.', () => {
+      const value = 0.5
+
+      expect(clamp(value, min, max)).toBe(value)
+    })
+  })
+
+  describe('range is from -100 to 100', () => {
+    const min = -100
+    const max = 100
+
+    test('value is less than 0.', () => {
+      expect(clamp(-200, min, max)).toBe(min)
+    })
+  
+    test('value is more than 1.', () => {
+      expect(clamp(200, min, max)).toBe(max)
+    })
+
+    test('value is within the range.', () => {
+      const value = 10
+
+      expect(clamp(value, min, max)).toBe(value)
+    })
+  })
 })
 
 test('degrees', () => {
